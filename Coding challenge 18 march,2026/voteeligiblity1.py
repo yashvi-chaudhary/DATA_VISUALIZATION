065a05069a212b93b0fd618168d8979027116d70
Yashvi Chaudhary <chaudharyyashvi67@gmail.com>
2026-03-18 10:14:51 +0530

    Program to check vote eligibility using a different logic

---
+++ Coding challenge 18 march,2026/voteeligiblity1.py
@@ -0,0 +1,10 @@
+# Program to check vote eligibility using a different logic
+
+# Taking input from the user
+age = int(input("Enter your age: "))
+
+# Using conditional expression (ternary operator)
+result = "Eligible to vote" if age >= 18 else "Not Eligible to vote"
+
+# Printing the result
+print(result)
